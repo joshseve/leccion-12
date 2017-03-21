ebaabdb62c70ca6495291a4ccdaa35b564827ff0
Paola Jocelin Septimo Velazquez <Paola Jocelin Septimo Velazquez>
2017-03-21 08:30:43 -0600

    Lo que hasta ahora logré hacer para el ejercicio de Frutas

---
+++ convertidorGrados.js
@@ -0,0 +1,14 @@
+function convertirACelsius (){
+ var gradosFahrenheit= parseInt(document.getElementById("fahrenheit").value);
+ var resultado = document.getElementById("celsius");
+ var celsius = parseFloat((gradosFahrenheit - 32) * (5/9)).toFixed(2);
+   resultado.value = celsius;
+}
+
+function convertirAFahrenheit (){
+ var gradosCelsius = parseInt (document.getElementById("celsius").value);
+ var resul = document.getElementById("fahrenheit");
+ var fahrenheit = parseFloat((gradosCelsius * 1.8) + 32).toFixed(2);
+   resul.value = fahrenheit;
+
+}
---
+++ valores.js
@@ -0,0 +1,7 @@
+function calcularArea(){
+  var area = document.getElementById('area').value;
+  var resultado = Math.PI * parseInt(area);
+
+  document.getElementById('resultadoArea').value = resultado
+
+}
